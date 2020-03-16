9ce373aaa3cb1c7ce13a05d10bc98a2c538ea71a
buihoang <hoangbuivan0962654936@gmail.com>
2020-03-16 13:27:40 +0700

    first commit

---
+++ model/DBConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Model;
+
+use PDO;
+
+class DBConnection
+{
+    public $dns;
+    public $username;
+    public $password;
+    
+    public function __construct($dns, $username, $password)
+    {
+        $this->dns = $dns;
+        $this->username = $username;
+        $this->password = $password;
+    }
+    
+    public function connect()
+    {
+        return new PDO($this->dns,$this->username,$this->password);
+    }
+}
